4bcb0db2373cf7c186ae5e8bc5e4bed4a35cd758
LAPTOP-G62JATNB <istiyak98215@gmail.com>
2021-06-19 15:52:08 +0530

    branch1 commit

--- lib/Pages/login.dart
+++ lib/Pages/login.dart
@@ -1,34 +1,60 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorials/utills/routes.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool isLogin = false;
+  String _name = "";
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Scaffold(
-        body: SingleChildScrollView(
+      child: SafeArea(
+        child: Scaffold(
+            body: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Image.asset("assets/images/login_img.png", fit: BoxFit.fitWidth,height: 250, width: MediaQuery.of(context).size.width,),
+              Image.asset(
+                "assets/images/login_img.png",
+                fit: BoxFit.fitWidth,
+                height: 250,
+                width: MediaQuery.of(context).size.width,
+              ),
               SizedBox(
                 height: 100,
-                child: Center(child: Text("Welcome", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)),
+                child: Center(
+                    child: Text(
+                      "Welcome  $_name",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                )),
               ),
               Padding(
-                padding: EdgeInsets.all(20),
+                padding: const EdgeInsets.all(20),
                 child: Column(
                   children: [
-                   TextFormField(
-                     decoration: const InputDecoration(
-                       hintText: "Enter Name",
-                       labelText: "Name",
-                       prefixIcon: Icon(Icons.accessibility_new),
-                     ),
-                   ),
-                    SizedBox(height: 20,),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Enter Name",
+                        labelText: "Name",
+                        prefixIcon: Icon(Icons.accessibility_new),
+                      ),
+                      onChanged: (value){
+                        _name = value;
+                        setState(() {
+
+                        });
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
                     TextFormField(
                       obscureText: true,
                       decoration: const InputDecoration(
@@ -37,19 +63,47 @@ class Login extends StatelessWidget {
                         prefixIcon: Icon(Icons.attach_email),
                       ),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 50,
                     ),
-                    ElevatedButton(onPressed: ()=> print("Login Successfully"),
-                        child: Text("Login",),
-                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                    )
-                 ],),
+                    // ElevatedButton(onPressed: ()=> Navigator.pushNamed(context, MyRoutes.HomePage),
+                    //     child: const Text("Login",),
+                    //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    // ),
+                    InkWell(
+                      onTap:() async{
+                        setState(()  {
+                          isLogin = true;
+                        });
+                        await Future.delayed(Duration(seconds: 5));
+                        Navigator.pushNamed(context, MyRoutes.HomePage);
+                      },
+                      child: AnimatedContainer(
+                        alignment: Alignment.center,
+                        duration: const Duration(seconds: 5),
+                        width: isLogin? 50 :150,
+                        height: isLogin ? 50 :50,
+                        child: isLogin
+                            ? const Icon(CupertinoIcons.check_mark, color: Colors.white,)
+                            : const Text(
+                                "Login",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius: isLogin?BorderRadius.circular(50): BorderRadius.circular(5),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               )
-
             ],
           ),
-        )
+        )),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tutorials/Pages/login.dart';
+import 'package:flutter_tutorials/utills/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'Pages/home.dart';
 
@@ -13,10 +15,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       initialRoute: "/",
       routes: {
         '/': (context)=>Login(),
-        '/home': (context)=>Home(),
+        MyRoutes.HomePage : (context)=>Home(),
+        MyRoutes.LoginPage : (context)=>Login(),
       },
     );
 
