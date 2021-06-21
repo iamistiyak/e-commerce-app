364895a3aec986369342cf9b51bc26e8670fa9e6
LAPTOP-G62JATNB <istiyak98215@gmail.com>
2021-06-21 15:49:36 +0530

    day 5 - Create ListView builder and set value using ListTile

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorials/models/catalog.dart';
+import 'package:flutter_tutorials/widget/item_builder.dart';
 import 'package:flutter_tutorials/widget/mydrawer.dart';
 
 class Home extends StatelessWidget {
+  final itemListRandomGenerated = List.generate(5, (index) =>CatalogModel.items[0] );
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -10,11 +14,12 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Center(child: Text("Istiyak" ,style: TextStyle(color: Colors.deepPurple),)),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Hello Istiyak"),
-        ),
-      ),
+      body: ListView.builder(
+          itemCount: itemListRandomGenerated.length,
+          itemBuilder: (context, index){
+            return MyItemBuilder(item: itemListRandomGenerated[0],);
+          }
+          ),
       drawer: MyDrawer(),
     );
   }
